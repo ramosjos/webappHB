3d1df9a399a8f3958320794447efd0eb21e06178
ramosjos <ramosjos@oregonstate.edu>
2019-12-08 18:46:50 -0800

    Fixed home page select queries

--- home.js
+++ home.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getHomes(res, mysql, context, complete){
-        mysql.pool.query("SELECT games.id, teams1.team_name AS team_1_name, results.team_1_score, teams2.team_name AS team_2_name, results.team_2_score, results.quarter, results.time_left FROM games INNER JOIN results ON games.id = results.game_id INNER JOIN teams AS teams1 ON results.team_1_id = teams1.id INNER JOIN teams AS teams2 ON results.team_2_id = teams2.id WHERE (results.time_left <> ('00:12:00') AND results.quarter <> '1') AND (results.time_left <> ('00:00:00') AND results.quarter <> '4')  ORDER BY id ASC", function(error, results, fields){
+        mysql.pool.query("SELECT games.id, teams1.team_name AS team_1_name, results.team_1_score, teams2.team_name AS team_2_name, results.team_2_score, results.quarter, results.time_left FROM games INNER JOIN results ON games.id = results.game_id INNER JOIN teams AS teams1 ON results.team_1_id = teams1.id INNER JOIN teams AS teams2 ON results.team_2_id = teams2.id WHERE !(results.time_left = ('00:12:00') AND results.quarter = '1') AND !(results.time_left = ('00:00:00') AND results.quarter = '4')  ORDER BY id ASC", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -15,7 +15,7 @@ module.exports = function(){
 
     function getHomesSearch(req, res, mysql, context, complete){
 
-        var query = "SELECT games.id, teams1.team_name AS team_1_name, results.team_1_score, teams2.team_name AS team_2_name, results.team_2_score, results.quarter, results.time_left FROM games INNER JOIN results ON games.id = results.game_id INNER JOIN teams AS teams1 ON results.team_1_id = teams1.id INNER JOIN teams AS teams2 ON results.team_2_id = teams2.id WHERE (results.time_left <> ('00:12:00') AND results.quarter <> '1') AND (results.time_left <> ('00:00:00') AND results.quarter <> '4') AND (teams1.team_name LIKE " + mysql.pool.escape(req.params.s + '%') + " OR teams2.team_name LIKE " + mysql.pool.escape(req.params.s + '%') + ") ORDER BY id ASC";
+        var query = "SELECT games.id, teams1.team_name AS team_1_name, results.team_1_score, teams2.team_name AS team_2_name, results.team_2_score, results.quarter, results.time_left FROM games INNER JOIN results ON games.id = results.game_id INNER JOIN teams AS teams1 ON results.team_1_id = teams1.id INNER JOIN teams AS teams2 ON results.team_2_id = teams2.id WHERE !(results.time_left = ('00:12:00') AND results.quarter = '1') AND !(results.time_left = ('00:00:00') AND results.quarter = '4') AND (teams1.team_name LIKE " + mysql.pool.escape(req.params.s + '%') + " OR teams2.team_name LIKE " + mysql.pool.escape(req.params.s + '%') + ") ORDER BY id ASC";
 			mysql.pool.query(query, function(error, results, fields){
 			if(error){
 				res.write(JSON.stringify(error));
